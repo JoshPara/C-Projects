696e98987b559822171af5209433166e5327715e
Josh Paradie <98863349+JoshPara@users.noreply.github.com>
2022-03-31 13:26:44 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,10 @@
+// This is a simple program that will output my name, student ID and email address
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Joshua Paradie\n");
+    printf("00648731\n");
+    printf("jpara2@unh.newhaven.edu\n");
+    return 0;
+}
